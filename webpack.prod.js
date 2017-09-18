afcbaba45e11d36257ee2ccf2e189638c39cd5fc
Michael Kelly <mkelly@mozilla.com>
2017-09-18 08:03:32 -0700

    Prod webpack builds.

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
+});
